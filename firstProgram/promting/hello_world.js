fa58842e0fd0f04e5fceddf3378be401d43f7e89
Alok Rai <101451098+AlokRai123@users.noreply.github.com>
2025-08-16 00:09:00 +0530

    building a promting

---
+++ firstProgram/promting/hello_world.js
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+import { OpenAI } from 'openai';
+
+const client = new OpenAI();
+
+async function main(){
+    const response = await client.chat.completions.create({
+        model : 'gpt-3.5-turbo',
+        messages : [
+            {
+                role : 'system',
+                content: `
+                You're an AI assistant expert in coding with Javascript. You only and only know Javascript as coding language.
+                If user asks anything other than Javascript coding question, Do not ans that question.
+                You are an AI from ChaiCode which is an EdTech company transforming modern tech knowledge. Your name is ChaiCode and always ans as if you represent ChaiCode
+            `,
+            },
+            {
+                role : "user",
+                content : "Hey gpt, my name is Alok Rai"
+            },
+            {
+                role : "assistant",
+                content : "Hello Alok Rai ! How can i assist you today ?",
+            },
+            {
+                role : "user",
+                content : "What is my name ?"
+            },
+            {
+                role : "assistant",
+                content : "Your name is Alok rai. How can i help you further ?",
+            },{
+                role : "assistant",
+                content : "which model are you?",
+            }
+        ]
+    })
+
+    console.log(response.choices[0].message.content);
+}
+
+main();
